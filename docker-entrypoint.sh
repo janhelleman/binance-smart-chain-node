36edf149659378d97c868b3cc5135715d34de25d
Jan <227880+janhelleman@users.noreply.github.com>
2021-05-23 18:40:01 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,15 +9,21 @@ fi
 
 if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bsc" ]; then
 
+  if [ ! -d $BSC_CONFIGDIR/config ]; then
+    mkdir -p "$BSC_CONFIGDIR"
+    chmod 700 "$BSC_CONFIGDIR"
+    bsc --nousb --datadir "$BSC_DATADIR" init $BSC_CONFIGDIR/genesis.json
+    MV /config.toml "$BSC_CONFIGDIR"/config.toml
+  fi
+  
   if [ ! -d $BSC_DATADIR/geth/chaindata ]; then
     mkdir -p "$BSC_DATADIR"
     chmod 700 "$BSC_DATADIR"
-    bsc --nousb --datadir "$BSC_DATADIR" init $BSC_CONFIGDIR/genesis.json
   fi
 
   echo "$0: setting data directory to $BSC_DATADIR"
 
-  set -- "$@" --datadir "$BSC_DATADIR" --config $BSC_CONFIGDIR/config.toml
+  set -- "$@" --datadir "$BSC_DATADIR" --config "$BSC_CONFIGDIR/config.toml"
 fi
 
 echo
